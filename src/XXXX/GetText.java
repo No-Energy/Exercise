ea55e81a31500e08fa7ea54da3254ed8b2ea0e0a
No-Energy <No_Energy@outlook.com>
2015-11-24 07:35:20 +0800

    c

--- src/XXXX/GetText.java
+++ src/XXXX/GetText.java
@@ -1,5 +1,37 @@
 package XXXX;
 
-public class GetText {
+import java.util.Scanner;
 
+public class GetText {
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		
+		System.out.println("input text");
+		String str =s.nextLine();
+		System.out.println("input password");
+		String pwd =s.nextLine();
+		s.close();
+		int[] revert_int = new int[pwd.length()];
+        for (int i = 0; i < pwd.length(); i++)
+        {
+            revert_int[i] = Integer.parseInt(String.valueOf(pwd.charAt(i)));
+        }
+        int start_num = 0;
+        String revert_str = "";
+        for (int i=0;i<str.length();i++)
+        {
+        	char c=str.charAt(i);
+            int cint = revert_int[start_num];
+            if (start_num == (pwd.length() - 1))
+            {
+                start_num = 0;
+            }
+            else
+            {
+                start_num++;
+            }
+            revert_str += ((char)((int)c - cint));
+        }
+        System.out.println(revert_str);
+	}
 }
