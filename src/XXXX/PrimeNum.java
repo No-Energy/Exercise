e0578396d9d783a135a4a596412fd43d2f19638e
No-Energy <no_energy@outlook.com>
2015-12-17 10:53:51 +0800

    edit

--- src/XXXX/PrimeNum.java
+++ src/XXXX/PrimeNum.java
@@ -11,21 +11,26 @@ public class PrimeNum {
 		int num =s.nextInt();
 		
 		//int sum=0;
-		System.out.println(2);
 		
-		for(int i=3;i<num;i++){
-			if (i%2>0){
-				boolean isPrime=true;
-				for(int j=3;j<i;j+=2){
-					if(i%j==0){isPrime=false;break;}
-				}
-				if(isPrime){
-					//sum+=i;
-					System.out.println(i);
+		if (num<=2){
+			System.out.println("plz input num > 2");
+		}
+		else{
+			System.out.println(2);	
+			for(int i=3;i<num;i++){
+				if (i%2>0){
+					boolean isPrime=true;
+					for(int j=3;j<i;j+=2){
+						if(i%j==0){isPrime=false;break;}
+					}
+					if(isPrime){
+						//sum+=i;
+						System.out.println(i);
+					}
 				}
 			}
+			//System.out.println(sum);
 		}
-		//System.out.println(sum);
 		s.close();
 	}
 }
